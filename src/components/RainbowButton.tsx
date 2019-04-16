a974468523c956b55506739cab1190d9dff84604
Vojtech Rinik <vojto@rinik.net>
2019-04-16 09:15:15 +0200

    Added basic config using Typescript and webpack

---
+++ src/components/RainbowButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const RainbowButton: React.FC = () => {
+  return (
+    <button>RAINBOW!</button>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "none",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  externals: [nodeExternals()],
+  context: path.resolve(__dirname, "src"),
+  entry: ".",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'awesome-typescript-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[local]_[hash:base64:5]"
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: 'umd'
+  }
+};
